19168812d2dd2525c372cc5b08e631b9ee03983d
Ignacio Lapenta <ignaciolapenta@gmail.com>
2023-07-07 14:52:08 -0300

    1

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,6 @@ export default function RootLayout({ children }) {
       
         {children}
       
-        <CardWrapper />
       </body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,30 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import './page.css'
 
 export default function Home() {
   return (
     <main className='main'>
-     
-    </main>
+      <div className="left">
+      <div className="nombre">
+        <h1>Cocinas del mundo</h1>
+      </div>
+      <div className="introduccion">
+        <p>Bienvenido/a a nuestro restaurante con platos de España, Francia e Italia. Aquí encontrarás una amplia
+          variedad de platos típicos de estos países, una breve descripción y la posibilidad de pedirlos para que los
+          llevemos a tu casa.</p>
+      </div>
+      <div className="button">
+        <div>
+          <a href="./pages/nosotros.html">Ver más</a>  
+        </div>
+      </div>
+    </div>
+    <div className="right">
+      <div className="imagen">
+        <img src="./assets/cheff.png" alt="chef"/>
+      </div>
+    </div>
+  </main>
+    
   )
 }
