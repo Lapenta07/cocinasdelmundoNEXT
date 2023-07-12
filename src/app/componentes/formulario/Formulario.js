00c120bcea364f7861d2c71bda969dc4e9f27362
Ignacio Lapenta <ignaciolapenta@gmail.com>
2023-07-12 15:51:31 -0300

    form2

--- src/app/componentes/formulario/Formulario.js
+++ src/app/componentes/formulario/Formulario.js
@@ -1,13 +1,14 @@
+import "./formulario.css"
 export default function Formulario () {
 
     return (
-        <div>
-            <form name="contact" method="POST" data-netifly="true" className="Form">
+        <div className="formulario">
+            <form name="contact" method="POST" data-netifly="true">
                 <input type="hidden" name="form-name" value="contact"></input>
                 <input type="text" id="nombre" name="nombre" placeholder="Nombre"></input>
                 <input type="text" id="apellido" name="apellido" placeholder="Apellido"></input>
                 <input type="text" id="mail" name="mail" placeholder="Email"></input>
-                <textarea id="mensaje" id="mensaje" name="mensaje" placeholder="Escriba su mensaje aqui..."></textarea>
+                <textarea id="mensaje" name="mensaje" placeholder="Escriba su mensaje aquí..."></textarea>
                 <button type="submit">ENVIAR</button>
             </form>
         </div>
